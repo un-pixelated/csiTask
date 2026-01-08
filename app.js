a4eadcf85df67bb47a6022c9b46602f13f9db852
Hardik Chona <masterhc321@gmail.com>
2026-01-08 20:16:34 +0530

    Refined grid button, added "infinite" page

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+const viewport = document.getElementById("viewport");
+const world = document.getElementById("world");
+
+let camX = -4000;
+let camY = -4000;
+let scale = 1;
+
+function updateCamera() {
+  world.style.transform = `translate(${camX}px, ${camY}px) scale(${scale})`;
+}
+
+updateCamera();
+
+let isPanning = false;
+let lastX = 0;
+let lastY = 0;
+
+viewport.addEventListener("mousedown", (e) => {
+  if (e.button !== 0) return;
+  isPanning = true;
+  lastX = e.clientX;
+  lastY = e.clientY;
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (!isPanning) return;
+
+  const dx = e.clientX - lastX;
+  const dy = e.clientY - lastY;
+
+  lastX = e.clientX;
+  lastY = e.clientY;
+
+  camX += dx;
+  camY += dy;
+
+  updateCamera();
+});
+
+window.addEventListener("mouseup", () => {
+  isPanning = false;
+});
+
 const board = document.getElementById("board");
 const gridButton = document.getElementById("gridButton");
 
@@ -9,7 +52,7 @@ gridButton.addEventListener("mouseenter", () => {
 
   hoverTimer = setTimeout(() => {
     board.classList.add("showGrid");
-  }, 500);
+  }, 200);
 });
 
 gridButton.addEventListener("mouseleave", () => {
